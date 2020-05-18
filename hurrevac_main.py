45b30388795ef636b1f1bcceecc719cf672e85f5
NiyamITCLindeman <47092840+NiyamITCLindeman@users.noreply.github.com>
2020-05-18 08:57:53 -1000

    Add files via upload

--- hurrevac_main.py
+++ hurrevac_main.py
@@ -5,108 +5,72 @@ Requirements: Python 3.7, Anaconda3 64bit
 @author: Colin Lindeman
 """
 
+import tkinter as tk
+import tkinter.ttk as ttk
 import pandas as pd
-import numpy as np
-import urllib
 import json
-import statistics
-import math
-from math import radians, cos, sin, asin, sqrt 
-import pyodbc
 from sqlalchemy import create_engine
-import hurrevac_process_json
+
+def popupmsg(msg):
+    tk.messagebox.showinfo(message=msg)
+
+# def popupmsg(msg):
+#     NORM_FONT= ("Tahoma", 12)
+#     popup = tk.Toplevel()
+#     popup.wm_title("!")
+    
+#     # # Gets the requested values of the height and width.
+#     # windowWidth = popup.winfo_reqwidth()
+#     # windowHeight = popup.winfo_reqheight()
+#     # print("Width",windowWidth,"Height",windowHeight)
+#     # # Gets both half the screen width/height and window width/height
+#     # positionRight = int(popup.winfo_screenwidth()/2 - windowWidth/2)
+#     # positionDown = int(popup.winfo_screenheight()/3 - windowHeight/2)
+#     # # Positions the window in the center of the page.
+#     # popup.geometry("+{}+{}".format(positionRight, positionDown))
+    
+#     label = ttk.Label(popup, text=msg, font=NORM_FONT)
+#     label.pack(side="top", fill="x", pady=10)
+#     B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+#     B1.pack()
+#     popup.mainloop()
+
+def popupmsgNextSteps(msg):
+    NORM_FONT= ("Tahoma", 12)
+    popup = tk.Toplevel()
+    popup.grab_set()
+    popup.wm_title("!")
+    popup.resizable(0,0)
+    # Gets the requested values of the height and width.
+    windowWidth = popup.winfo_reqwidth()
+    windowHeight = popup.winfo_reqheight()
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(popup.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(popup.winfo_screenheight()/3 - windowHeight/2)
+    # Positions the window in the center of the page.
+    popup.geometry("+{}+{}".format(positionRight, positionDown))
+    
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.grid(row=1,column=0,padx=10,pady=10)
+    try:
+        #global img_NextSteps
+        img_NextSteps = tk.PhotoImage(file="./images/NextSteps.png")
+        imageLabel = tk.Label(popup, image=img_NextSteps)
+        imageLabel.image = img_NextSteps
+        imageLabel.grid(row=2,column=0,padx=10,pady=10)
+    except Exception as e:
+        print(e)
+    
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.grid(row=3,column=0,padx=10,pady=10)
+    popup.mainloop()
 
 def get_key(val, my_dict): 
+    '''Currently not used'''
     for key, value in my_dict.items(): 
          if val == value: 
              return key 
     return "key doesn't exist"
-
-class StormsInfo:
-    def GetStormsJSON(self):
-        with open(r".\hurrevac_settings.json") as f:
-            hurrevacSettings = json.load(f)
-        openUrl = urllib.request.urlopen(hurrevacSettings['HurrevacStormsURL'])
-        
-        if(openUrl.getcode()==200):
-            stormsdata = openUrl.read()
-            stormsJSON = json.loads(stormsdata)
-            self.JSON = stormsJSON
-        else:
-            print("Error receiving data", openUrl.getcode())
-
-    def GetStormsBasins(self):
-        #working with json
-        # this is needed to filter storm ids for user
-        with open(r".\hurrevac_settings.json") as f:
-            hurrevacSettings = json.load(f)
-        StormBasins = hurrevacSettings['BasinsDictionary']
-        StormBasinsLabels = list(StormBasins.values())
-        StormBasinsLabels.sort()
-        self.basins = tuple(StormBasinsLabels)
-
-    def GetStormsYears(self):
-        # this is needed to filter storm ids for user
-        #working with json
-        yearList = []
-        for i in self.JSON:
-            try:
-                year = int(i)
-            except:
-                year = i
-            yearList.append(year)
-        yearList.sort(reverse=True)
-        self.years = tuple(yearList)
-    
-    def GetStormLabels(self, basinLabel, year):
-        with open(r".\hurrevac_settings.json") as f:
-            hurrevacSettings = json.load(f)
-        StormBasins = hurrevacSettings['BasinsDictionary']
-        basin = get_key(basinLabel, StormBasins)
-        #working with json
-        stormNameIdStatusList = []
-        try:
-            year = str(year)
-        except:
-            year = year
-        for storm in self.JSON[year]:
-            stormName = str(storm['name'])
-            stormId = str(storm['stormId'])
-            stormStatus = str(storm['status'])
-            stormBasin = str(storm['basin'])
-            # stormDict = {'stormName':stormName, \
-            #              'stormId':stormId, \
-            #              'stormStatus':stormStatus}
-            # stormNameIdStatusList.append(stormDict)
-            stormLabel = stormName + " (" + stormStatus + ") " + " [" + stormId + "]"
-            if stormBasin == basin and stormStatus != "Simulated":
-                stormNameIdStatusList.append(stormLabel)
-            stormNameIdStatusList.sort()
-        return tuple(stormNameIdStatusList)
-
-class StormInfo:
-    def GetStormJSON(self, StormId):
-        self.Id = StormId
-        #from internet
-        #attribute and used as input to GetStormDataframe
-        with open(r".\hurrevac_settings.json") as f:
-            hurrevacSettings = json.load(f)
-        url = hurrevacSettings['HurrevacStormURL'] + "/" + StormId
-        openUrl = urllib.request.urlopen(url)
-        if(openUrl.getcode()==200):
-            stormdata = openUrl.read()
-            stormJSON = json.loads(stormdata)
-            self.JSON = stormJSON
-        else:
-            print("Error receiving data", openUrl.getcode())
-
-    def GetStormDataframe(self, stormJSON):
-        #from other python script
-        #attribute and used as input to ExportToJSON
-        stormDataframes = hurrevac_process_json.processStormJSON(stormJSON)
-        self.huScenarioName = stormDataframes[0]
-        self.huScenario = stormDataframes[1]
-        self.huStormTrack = stormDataframes[2]
 
 #Tools to export data
 def ExportToJSON(inputDataFrame, outputPath):
@@ -126,6 +90,7 @@ def CheckScenarioName(huScenarioName):
     if huScenarioName not in scenariosList:
         return True
     else:
+        popupmsg(f'Scenario "{huScenarioName}" already exists.\nPlease use "Hazus Hurricane Scenario Wizard" to edit or delete.')
         return False
 
 def ExportToHazus(huScenarioName, huScenario, huStormTrack):
@@ -134,45 +99,26 @@ def ExportToHazus(huScenarioName, huScenario, huStormTrack):
     server = hurrevacSettings['HazusServerName']
     userName = hurrevacSettings['HazusUserName']
     password = hurrevacSettings['HazusPassword']
-    engine = create_engine('mssql+pyodbc://'+userName+':'+password+'@'+server+'/syHazus?driver=SQL+Server')
-    conn = engine.connect()
+    try:
+        engine = create_engine('mssql+pyodbc://'+userName+':'+password+'@'+server+'/syHazus?driver=SQL+Server')
+        conn = engine.connect()
+    except:
+        popupmsg("Error connecting to Hazus SQL Server. Check your Settings.json")
     huScenarioDoesntExist = CheckScenarioName(huScenarioName)
     if huScenarioDoesntExist:
-        huScenario.to_sql(name="huScenario", con=conn, if_exists='append', index=False)
-        huStormTrack.to_sql(name="huStormTrack", con=conn, if_exists='append', index=False)
-        return "scenario does not exist, proceed"
-    else:
-        #inform user that scenarioname already exists and how to resolve it, popup window
-        return "either scenario already exists or to_sql failed"
-    
-#Test some of the code above...
+        print(f'scenario "{huScenarioName}" does not exist in huScenario, proceed')
+        try:
+            huScenario.to_sql(name="huScenario", con=conn, if_exists='append', index=False)
+            huStormTrack.to_sql(name="huStormTrack", con=conn, if_exists='append', index=False)
+            popupmsgNextSteps(f'''Scenario "{huScenarioName}" is now available in Hazus.
+                  
+Please build or open an existing region and:
+1.	Select “{huScenarioName}”
+2.	Choose “Edit”  so that Hazus will check and validate imported data.
+3.	Select Next and proceed through Hazus wizard until new scenario is saved.''')
+        except:
+            popupmsg(f"Error loading {huScenarioName} into Hazus.")
+
+# #Test some of the code above...
 if __name__ == "__main__":
-    myclass = StormsInfo()
-    myclass.GetStormsJSON()
-    myclass.GetStormsBasins()
-    myclass.GetStormsYears()
-    
-    print("all possible basins from config:", myclass.basins)
-    print()
-    print("all possible years from source:", myclass.years)
-    print()
-    print("Atlantic 2014 storms:", myclass.GetStormLabels('Atlantic', '2014'))
-    print()
-    print("Eastern Pacific 2020 storms:", myclass.GetStormLabels('Eastern Pacific', '2020'))
-    print()
-    
-    storm1 = StormInfo()
-    storm1.GetStormJSON("al032016")
-    print(storm1.Id)
-    #print(storm1.JSON)
-    print(storm1.Id)
-    storm1.GetStormDataframe(storm1.JSON)
-    print(storm1.huScenario)
-    print(storm1.huStormTrack)
-    
-    print(CheckScenarioName("HARVEY_2017_stm_2204PM"))
-    print(CheckScenarioName("HARVEY - Adv 53 AL2018"))
-    #print(ExportToHazus(storm1.huScenarioName, storm1.huScenario, storm1.huStormTrack))
-    ExportToHazus(storm1.Id, storm1.huScenario, storm1.huStormTrack)
-    
-    
+    popupmsgNextSteps("Test error message")
---
+++ hurrevac_storms.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 09:36:01 2020
+Requirements: Python 3.7, Anaconda3 64bit
+@author: Colin Lindeman
+"""
+
+import tkinter as tk
+import tkinter.ttk as ttk
+import urllib
+import json
+import hurrevac_storm
+
+def popupmsg(msg):
+    NORM_FONT= ("Tahoma", 12)
+    popup = tk.Toplevel()
+    popup.wm_title("!")
+    # Gets the requested values of the height and width.
+    windowWidth = popup.winfo_reqwidth()
+    windowHeight = popup.winfo_reqheight()
+    # Gets both half the screen width/height and window width/height
+    positionRight = int(popup.winfo_screenwidth()/2 - windowWidth/2)
+    positionDown = int(popup.winfo_screenheight()/3 - windowHeight/2)
+    # Positions the window in the center of the page.
+    popup.geometry("+{}+{}".format(positionRight, positionDown))
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
+def get_key(val, my_dict): 
+    for key, value in my_dict.items(): 
+         if val == value: 
+             return key 
+    return "key doesn't exist"
+
+class StormsInfo:
+    def __init__(self):
+        ''' '''
+        self.GetStormsJSON()
+        self.GetStormsTypes()
+        self.GetStormsYears()
+        self.GetStormsBasins()
+    
+    def GetStormsJSON(self):
+        with open(r".\hurrevac_settings.json") as f:
+            hurrevacSettings = json.load(f)
+        openUrl = urllib.request.urlopen(hurrevacSettings['HurrevacStormsURL'])
+        
+        if(openUrl.getcode()==200):
+            stormsdata = openUrl.read()
+            stormsJSON = json.loads(stormsdata)
+            self.JSON = stormsJSON
+        else:
+            print("Error receiving data", openUrl.getcode())
+            
+    def GetStormsTypes(self):
+        #working with json
+        with open(r".\hurrevac_settings.json") as f:
+            hurrevacSettings = json.load(f)
+        StormTypes = hurrevacSettings['ShowStormTypes']
+        StormTypesList = []
+        for key in StormTypes.keys():
+            if StormTypes[key]:
+                StormTypesList.append(key)
+        StormTypesList.sort()
+        self.types = tuple(StormTypesList)
+
+    def GetStormsBasins(self):
+        #working with json
+        with open(r".\hurrevac_settings.json") as f:
+            hurrevacSettings = json.load(f)
+        StormBasins = hurrevacSettings['BasinsDictionary']
+        StormBasinsLabels = list(StormBasins.values())
+        self.basins = tuple(StormBasinsLabels)
+
+    def GetStormsYears(self):
+        #working with json
+        yearList = []
+        for i in self.JSON:
+            try:
+                year = int(i)
+            except:
+                year = i
+            yearList.append(year)
+        yearList.sort(reverse=True)
+        self.years = tuple(yearList)
+    
+    def GetStormNames(self, stormTypes, basinLabel, year):
+        '''Get basins code from label in settings.json'''
+        with open(r".\hurrevac_settings.json") as f:
+            hurrevacSettings = json.load(f)
+        StormBasins = hurrevacSettings['BasinsDictionary']
+        basinCode = get_key(basinLabel, StormBasins)
+        #working with json
+        stormNameStatusIDList = []
+        activeStorms = []
+        historicStorms = []
+        exerciseStorms = []
+        simulatedStorms = []
+        '''Iterate over storms for a given year, append to each list'''
+        try:
+            year = str(year)
+        except:
+            year = year
+        for storm in self.JSON[year]:
+            stormName = str(storm['name'])
+            stormId = str(storm['stormId'])
+            stormStatus = str(storm['status'])
+            stormBasin = str(storm['basin'])
+            stormLabel = stormName + " (" + stormStatus + ") " + " [" + stormId + "]"
+            if stormBasin == basinCode:
+                if stormStatus == "Simulated":
+                    simulatedStorms.append(stormLabel)
+                elif stormStatus == "Historical":
+                    historicStorms.append(stormLabel)
+                elif stormStatus == "Exercise":
+                    exerciseStorms.append(stormLabel)
+                elif stormStatus == "Active":
+                    activeStorms.append(stormLabel)
+        activeStorms.sort()
+        historicStorms.sort()
+        exerciseStorms.sort()
+        simulatedStorms.sort()
+        '''Determine which type of storms are added to the main list'''
+        if 'Active' in stormTypes:
+            for i in activeStorms:
+                stormNameStatusIDList.append(i)
+        if 'Historical' in stormTypes:
+            for i in historicStorms:
+                stormNameStatusIDList.append(i)
+        if 'Exercise' in stormTypes:
+            for i in exerciseStorms:
+                stormNameStatusIDList.append(i)
+        if 'Simulated' in stormTypes:
+            for i in simulatedStorms:
+                stormNameStatusIDList.append(i)
+        if len(stormNameStatusIDList) > 0:
+            return tuple(stormNameStatusIDList)
+        else:
+            return ('No Storms Found',)
+    
+    def GetStormLabels(self, basinLabel, year):
+        '''This function is going away, may already be deprecated.'''
+        with open(r".\hurrevac_settings.json") as f:
+            hurrevacSettings = json.load(f)
+        StormBasins = hurrevacSettings['BasinsDictionary']
+        basin = get_key(basinLabel, StormBasins)
+        #working with json
+        stormNameIdStatusList = []
+        activeStorms = []
+        historicStorms = []
+        exerciseStorms = []
+        simulatedStorms = []
+        try:
+            year = str(year)
+        except:
+            year = year
+        for storm in self.JSON[year]:
+            stormName = str(storm['name'])
+            stormId = str(storm['stormId'])
+            stormStatus = str(storm['status'])
+            stormBasin = str(storm['basin'])
+            # stormDict = {'stormName':stormName, \
+            #              'stormId':stormId, \
+            #              'stormStatus':stormStatus}
+            # stormNameIdStatusList.append(stormDict)
+            stormLabel = stormName + " (" + stormStatus + ") " + " [" + stormId + "]"
+            if stormBasin == basin:
+                if stormStatus == "Simulated":
+                    simulatedStorms.append(stormLabel)
+                elif stormStatus == "Historical":
+                    historicStorms.append(stormLabel)
+                elif stormStatus == "Exercise":
+                    exerciseStorms.append(stormLabel)
+                elif stormStatus == "Active":
+                    activeStorms.append(stormLabel)
+        activeStorms.sort()
+        historicStorms.sort()
+        exerciseStorms.sort()
+        simulatedStorms.sort()
+        if hurrevacSettings['ShowStormTypes']['Active'] == 1:
+            for i in activeStorms:
+                stormNameIdStatusList.append(i)
+        if hurrevacSettings['ShowStormTypes']['Historical'] == 1:
+            for i in historicStorms:
+                stormNameIdStatusList.append(i)
+        if hurrevacSettings['ShowStormTypes']['Exercise'] == 1:
+            for i in exerciseStorms:
+                stormNameIdStatusList.append(i)
+        if hurrevacSettings['ShowStormTypes']['Simulated'] == 1:
+            for i in simulatedStorms:
+                stormNameIdStatusList.append(i)
+        return tuple(stormNameIdStatusList)
+
+class StormInfo:
+    def GetStormJSON(self, StormId):
+        self.Id = StormId
+        #from internet
+        #attribute and used as input to GetStormDataframe
+        with open(r".\hurrevac_settings.json") as f:
+            hurrevacSettings = json.load(f)
+        url = hurrevacSettings['HurrevacStormURL'] + "/" + StormId
+        openUrl = urllib.request.urlopen(url)
+        if(openUrl.getcode()==200):
+            #Need to check if response is 200 but there is no data "[]", ie a non-valid stormid request.
+            stormdata = openUrl.read()
+            stormJSON = json.loads(stormdata)
+            self.JSON = stormJSON
+        else:
+            popupmsg("Error receiving data. Check settings.json url or site is down or changed.")
+            print("Error receiving data", openUrl.getcode())
+
+    def GetStormDataframe(self, stormJSON):
+        #from other python script
+        #attribute and used as input to ExportToJSON
+        stormDataframes = hurrevac_storm.processStormJSON(stormJSON)
+        self.huScenarioName = stormDataframes[0]
+        self.huScenario = stormDataframes[1]
+        self.huStormTrack = stormDataframes[2]
+
+#Test some of the code above...
+if __name__ == "__main__":
+    myclass = StormsInfo()
+    # myclass.GetStormsJSON()
+    # myclass.GetStormsBasins()
+    # myclass.GetStormsYears()
+    
+    print("all possible types from config:", myclass.types)
+    print()
+    print("all possible basins from config:", myclass.basins)
+    print()
+    print("all possible years from source:", myclass.years)
+    print()
+    print("Atlantic 2014 storm labels:", myclass.GetStormLabels('Atlantic', '2014'))
+    print()
+    print("Eastern Pacific 2020 storm labels:", myclass.GetStormLabels('Eastern Pacific', '2020'))
+    print()
+    print("Atlantic 2014 storm names:", myclass.GetStormNames(['Active', 'Historical', 'Exercise', 'Simulated'], 'Atlantic', '2014'))
+    print()
+    print("Eastern Pacific 2020 storm names:", myclass.GetStormNames(['Active', 'Historical', 'Exercise', 'Simulated'], 'Eastern Pacific', '2020'))
+    print()
+
+    
+    storm1 = StormInfo()
+    storm1.GetStormJSON("al012020")
+    #print(storm1.Id)
+    #print(storm1.JSON)
+    print(storm1.Id)
+    storm1.GetStormDataframe(storm1.JSON)
+    print(storm1.huScenario)
+    print(storm1.huStormTrack)
+    
