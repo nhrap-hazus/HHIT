ca80617d41a6ca6e2fd28fc1b310883ab9578e16
NiyamITCLindeman <47092840+NiyamITCLindeman@users.noreply.github.com>
2020-05-19 13:30:54 -1000

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    Hurrevac Load Tool
+    ~~~~~
+    FEMA NiyamIT built tool to load Hurrevac HVX storm data into Hazus Database.
+
+    :copyright: © 2019 by FEMA's Natural Hazards and Risk Assesment Program.
+    :license: cc, see LICENSE for more details.
+    :author: Colin Lindeman; colinlindeman@hotmail.com, Colin Lindeman; clindeman@niyamit.com
+
+"""
+
+__version__ = '0.0.1'
