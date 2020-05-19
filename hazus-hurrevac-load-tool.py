6d893decc838c045ad9ec9ba00d1c0b93933e8a4
NiyamITCLindeman <47092840+NiyamITCLindeman@users.noreply.github.com>
2020-05-19 13:31:26 -1000

    Trying to get scripts to run the GUI to work

--- hazus-hurrevac-load-tool.py
+++ hazus-hurrevac-load-tool.py
@@ -1,9 +1,5 @@
 from subprocess import call
 import time
 
-try:
-    call('CALL conda.bat activate hazus_env & start /min python ./src/hurrevac_run.py', shell=True)
-    time.sleep(5)
-except:
-    print('something went wrong')
-    time.sleep(5)
+call('CALL conda.bat activate hazus_env & start /min python ./src/hurrevac_run.py', shell=True)
+time.sleep(5)
--- src/hurrevac_run.py
+++ src/hurrevac_run.py
@@ -5,5 +5,5 @@ try:
     check_call('CALL conda.bat activate hazus_env && python hurrevac_gui.py', shell=True)
     time.sleep(5)
 except:
-    check_call('activate hazus_env && python gui_program.py', shell=True)
+    check_call('activate hazus_env && python hurrevac_gui.py', shell=True)
     time.sleep(5)
