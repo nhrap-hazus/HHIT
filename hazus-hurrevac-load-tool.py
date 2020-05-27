f43a328cb108a17f5c2376950092f40364893b4d
NiyamITCLindeman <47092840+NiyamITCLindeman@users.noreply.github.com>
2020-05-26 14:18:42 -1000

    Update hazus-hurrevac-load-tool.py

--- hazus-hurrevac-load-tool.py
+++ hazus-hurrevac-load-tool.py
@@ -1,5 +1,5 @@
 from subprocess import call
 import time
 
-call('CALL conda.bat activate hazus_env & start /min python ./src/hurrevac_run.py', shell=True)
+call('CALL conda.bat activate hazus_env & start /min python ./src/hurrevac_gui.py', shell=True)
 time.sleep(5)
