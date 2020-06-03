aa6d467acac92d4e2fdacf16190123bfee0f4c37
NiyamITCLindeman <47092840+NiyamITCLindeman@users.noreply.github.com>
2020-06-03 11:58:03 -1000

    Update hurrevac_run.py

--- src/hurrevac_run.py
+++ src/hurrevac_run.py
@@ -1,9 +1,13 @@
-from subprocess import check_call
-import time
-
 try:
-    check_call('CALL conda.bat activate hazus_env && python hurrevac_gui.py', shell=True)
-    time.sleep(5)
+    from manage import internetConnected, checkForHazusUpdates, checkForToolUpdates
+    if internetConnected():
+        checkForHazusUpdates()
+        checkForToolUpdates()
+
+    from subprocess import check_call
+    check_call('CALL conda.bat activate hazus_env && python .\src\hurrevac_gui.py', shell=True)
+
 except:
-    check_call('activate hazus_env && python hurrevac_gui.py', shell=True)
-    time.sleep(5)
+    import ctypes
+    messageBox = ctypes.windll.user32.MessageBoxW
+    messageBox(0,"The tool was unable to open. You need internet connection for this tool to update. If this problem persists, contact hazus-support@riskmapcds.com","Hazus", 0x1000)
