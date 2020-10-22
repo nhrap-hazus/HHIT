ae206dee2f7ff1957c63330c20555aac7f8dab56
NiyamITCLindeman <clindeman@niyamit.com>
2020-10-21 17:37:25 -1000

    Updated from https://github.com/nhrap-dev/FAST/tree/dev1/src
    to match up manage and config updates; per UKS

--- src/update.py
+++ src/update.py
@@ -1,11 +1,23 @@
+##try:
+##    from manage import internetConnected, checkForHazPyUpdates, checkForToolUpdates
+##    checkForHazPyUpdates()
+##    checkForToolUpdates()
+##
+##except:
+##    import ctypes
+##    import sys
+##    messageBox = ctypes.windll.user32.MessageBoxW
+##    messageBox(0, "Unexpected error:" + sys.exc_info()[0] + " | If this problem persists, contact hazus-support@riskmapcds.com.", "HazPy",
+  
 try:
-    from manage import internetConnected, checkForHazPyUpdates, checkForToolUpdates
+    try:
+        from src.manage import internetConnected, checkForHazPyUpdates, checkForToolUpdates
+    except:
+        from manage import internetConnected, checkForHazPyUpdates, checkForToolUpdates
     checkForHazPyUpdates()
     checkForToolUpdates()
-
 except:
     import ctypes
     import sys
     messageBox = ctypes.windll.user32.MessageBoxW
-    messageBox(0, "Unexpected error:" + sys.exc_info()[0] + " | If this problem persists, contact hazus-support@riskmapcds.com.", "HazPy", 0x1000)
-   
+    messageBox(0, "Unexpected error:" + str(sys.exc_info()[0]) + " | If this problem persists, contact hazus-support@riskmapcds.com.", "HazPy", 0x1000)
